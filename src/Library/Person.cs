591ad14a3110168e38e5c33f3c23c69492a50f0b
Josue2kMR <josuernesto05@gmail.com>
2024-10-30 09:51:25 -0300

    agregar person

---
+++ src/Library/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
